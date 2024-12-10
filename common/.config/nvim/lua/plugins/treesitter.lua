4a82a33c2f796569dd4fc0dd14fbd04381b5c60b
suwa <suwuako@protonmail.com>
2024-12-11 02:23:17 +1100

    rice rofi

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,16 @@ return {
                 enable = true,
                 disable = {}
             },
+        vim.filetype.add {
+              extension = { rasi = 'rasi' },
+              pattern = {
+                ['.*/waybar/config'] = 'jsonc',
+                ['.*/mako/config'] = 'dosini',
+                ['.*/kitty/*.conf'] = 'bash',
+                ['.*/hypr/.*%.conf'] = 'hyprlang',
+              },
+            }
         })
+
     end,
 }
